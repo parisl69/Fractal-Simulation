d505f428d06a633b69e5af5d69452f19d5660e6f
Paris Lyritis <plyritis@gmail.com>
2021-06-20 14:11:05 +0300

    Added container points input field. Added description

--- app.js
+++ app.js
@@ -3,71 +3,28 @@ const canvasWidth = 700;
 const canvasHeight = 700;
 const bigPoint = true;
 const smallPoint = false;
-const containerPoints = 4;
+var containerPoints = 3;
 const randomPointColor = "red";
 const containterPointColor = "green";
 const pointColor = "lightblue";
 const simulationSteps = 100000;
-
 var canvas = document.getElementById("myCanvas");
 canvas.setAttribute("width", canvasWidth);
 canvas.setAttribute("height", canvasHeight);
 
-
-/**
- * Clears the canvas and draws a fresh new simulation
- */
-function drawSimulation() {
-
-    const context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvasWidth, canvasHeight);
-
-    //  Define a random container
-    var container = [];
-    for (i=0; i<containerPoints; i++) {
-        container.push(
-            [ randomInt(0,canvasWidth), randomInt(0,canvasHeight) ]
-        );
-    }
-
-    // Calculate the first random point for our simulation
-    var currentPoint = [
-        randomInt(0, canvasWidth),
-        randomInt(0, canvasHeight)
-    ];    
-    // Save this for later
-    var firstRandomPoint = currentPoint;
-    // Run the simulation for 10000 steps
-    for (j=0; j<simulationSteps; j++) {
-        const dice = randomInt(1,containerPoints);
-        // random triangle selected
-        const refPoint = container[Object.keys(container)[dice-1]];
-        // Declare new current point coordinates
-        let newX= (currentPoint[0] + refPoint[0])/2;
-        let newY= (currentPoint[1] + refPoint[1])/2; 
-        currentPoint = [ parseInt(newX), parseInt(newY) ];
-        drawPoint(currentPoint, smallPoint, "", pointColor);
-    }
-    // Draw the container points
-    var counter=1;
-    container.forEach(element => {
-        drawPoint(element, bigPoint, counter++, containterPointColor);
-    });
-
-    // Draw thw first point in random position
-    drawPoint(firstRandomPoint, bigPoint, "Random", randomPointColor);
-
-}
-
-
-// Run app on document load
 $( document ).ready(function() {
     
+    $("#numberOfContainerPoints").val(containerPoints);
     drawSimulation();
-    $("#recalculate").click(function(e){
+
+    $("#randomize").click(function(e){
         drawSimulation();
     });
 
+    $("#numberOfContainerPoints").change(function(e){
+        containerPoints = parseInt( $(this).val() );
+        drawSimulation();
+    });
 
 });
 
--- functions.js
+++ functions.js
@@ -28,6 +28,52 @@ function drawPoint( point, big=false, label="", color="black") {
     }
 }
 
+
+/**
+ * Clears the canvas and draws a fresh new simulation
+ */
+ function drawSimulation() {
+
+    let context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    //  Define a random container
+    var container = [];
+    for (i=0; i<containerPoints; i++) {
+        container.push(
+            [ randomInt(0,canvasWidth), randomInt(0,canvasHeight) ]
+        );
+    }
+
+    // Calculate the first random point for our simulation
+    var currentPoint = [
+        randomInt(0, canvasWidth),
+        randomInt(0, canvasHeight)
+    ];    
+    // Save this for later
+    var firstRandomPoint = currentPoint;
+    // Run the simulation for 10000 steps
+    for (j=0; j<simulationSteps; j++) {
+        const dice = randomInt(1,containerPoints);
+        // random triangle selected
+        const refPoint = container[Object.keys(container)[dice-1]];
+        // Declare new current point coordinates
+        let newX= (currentPoint[0] + refPoint[0])/2;
+        let newY= (currentPoint[1] + refPoint[1])/2; 
+        currentPoint = [ parseInt(newX), parseInt(newY) ];
+        drawPoint(currentPoint, smallPoint, "", pointColor);
+    }
+    // Draw the container points
+    var counter=1;
+    container.forEach(element => {
+        drawPoint(element, bigPoint, counter++, containterPointColor);
+    });
+
+    // Draw thw first point in random position
+    drawPoint(firstRandomPoint, bigPoint, "Start", randomPointColor);
+
+}
+
 /** Deprecated */
 
 
